5f3d5c3b501847fe443bc8da0337b5423eed48c0
Andrew Curry <andrew.d.curry@gmail.com>
2017-05-04 20:42:07 -0400

    Use third-party packages for map and isArray
    Because why not, this is Javascript.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import map from 'lodash.map'
+import isArray from 'lodash.isarray'
+
 /**
  * @param {Array<Array|Number|String|Object>}
  * @return {Array<Number|String|Object>}
  */
 export default function vlatten (array) {
-  return Array.isArray(array)
-    ? [].concat.apply([], array.map(vlatten))
+  return isArray(array)
+    ? [].concat.apply([], map(array, vlatten))
     : array;
 }
