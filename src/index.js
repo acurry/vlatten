ce91cdf939864abb22186195c189ab4d0eedc465
Andrew Curry <andrew.d.curry@gmail.com>
2017-05-04 20:32:46 -0400

    add main code and tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /**
- * @param {Type}
- * @return {Type}
+ * @param {Array<Array|Number|String|Object>}
+ * @return {Array<Number|String|Object>}
  */
-export default function () {
-  return true
+export default function vlatten (array) {
+  return Array.isArray(array)
+    ? [].concat.apply([], array.map(vlatten))
+    : array;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,38 @@
 import test from "tape"
 import vlatten from "../src"
 
-test("vlatten", (t) => {
-  t.plan(1)
-  t.equal(true, vlatten(), "return true")
+test("vlatten - simple cases", (t) => {
+  t.plan(3)
+
+  const expected = [1]
+
+  assertVlatten(t, [1], expected)
+
+  assertVlatten(t, [[1]], expected)
+
+  assertVlatten(t, [[[[[1]]]]], expected)
 })
+
+test("vlatten - complex cases", (t) => {
+  t.plan(2)
+
+  const expected = [1, 2, 3, 'a']
+
+  assertVlatten(t, [1, [2], [[3], ['a']]], expected)
+
+  assertVlatten(
+    t,
+    [[1], [[[2, [3], [], 'a']]]],
+    expected,
+    `handles empty array elements`
+  )
+})
+
+function assertVlatten(test, input, expected, message) {
+  message = message || `returns [${input}] as [${expected}]`;
+  test.deepEqual(
+    expected,
+    vlatten(input),
+    message
+  )
+}
